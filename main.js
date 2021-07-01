2d3c4e464fb1bdddfbc186d6855b9e73b7d8a889
Tulyakai <tulyawatt@gmail.com>
2021-07-01 14:48:57 +0700

    Mark#3

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data(){
         return{
             product: 'Boots',
-            description: 'This is description of boots!!!'
+            description: 'This is green boots!!!',
+            image: './assets/images/socks_green.jpg',
+            link: 'https://www.camt.cmu.ac.th'
         }
     }
 })
